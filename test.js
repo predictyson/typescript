1756f1c3f2c64ae9948836c262cef1a3f7f24863
yezgoget <yejison98@gmail.com>
2021-06-19 02:36:01 +0900

    initial settings

---
+++ test.js
@@ -0,0 +1,8 @@
+"use strict";
+var Test = /** @class */ (function () {
+    function Test() {
+        console.log('test');
+    }
+    return Test;
+}());
+new Test();
---
+++ test.ts
@@ -0,0 +1,6 @@
+class Test {
+    constructor() {
+        console.log('test');
+    }
+}
+new Test();
